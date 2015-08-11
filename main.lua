6fe6ea6ae1a7941041f5661867cda3061b7425b5
Sherjil Ozair <sherjilozair@gmail.com>
2015-08-11 15:28:04 -0700

    seems to be working now. Yet to train a good model.

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 require 'nn'
 require 'nngraph'
+require 'optim'
 
 unigrid = require 'unigrid'
 
@@ -9,22 +10,37 @@ cmd:text('Options')
 cmd:option('-size', 200,'rnn size')
 cmd:option('-k', 15, 'size of input bit vector')
 cmd:option('-n', 5, 'number of layers')
+cmd:option('-mb', 64, 'minibatch size')
+cmd:option('-iters', 100, 'number of iterations')
 opt = cmd:parse(arg)
 
 local x = nn.Identity()()
 
 local h0 = nn.Linear(opt.k, opt.size)(x)
 local m0 = nn.Linear(opt.k, opt.size)(x)
-local h, m = unigrid.unigrid(opt.size, opt.n, h0, m0)
+local hn, mn = unigrid.unigrid(opt.size, opt.n, h0, m0)
 
 
---local y = nn.CAddTable()({
-  --  nn.Linear(1, opt.size)(hn), 
-    --nn.Linear(1, opt.size)(mn)
---})
+local y = nn.Sigmoid()(nn.CAddTable()({
+    nn.Linear(opt.size, 1)(hn), 
+    nn.Linear(opt.size, 1)(mn)
+}))
 
-local model = nn.gModule({x}, {h, m})
---xx = torch.randn(opt.k)
---model:forward(xx)
+local model = nn.gModule({x}, {y})
+local criterion = nn.BCECriterion()
+local xx = torch.round(torch.rand(opt.mb, opt.k))
+local yy = torch.sum(xx, 2):apply(function(i) return math.fmod(i, 2)end)
 
-graph.dot(model.fg, 'mlp', 'mymlp')
+w, dw = model:getParameters()
+w:uniform(-0.08, 0.08)
+optim_state = {learningRate=1.}
+for i=1,opt.iters do
+    feval = function(w_new)
+        dw:zero()
+        local loss = criterion:forward(model:forward(xx), yy)
+        model:backward(xx, criterion:backward(model.output, yy))
+        return loss, dw
+    end
+    _, fs = optim.sgd(feval,w, optim_state)
+    print (fs[1])
+end 
