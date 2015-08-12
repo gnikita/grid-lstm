c0a6d6e7feb936ea8848723399f14c9c69dfa15d
Sherjil Ozair <sherjilozair@gmail.com>
2015-08-11 17:07:20 -0700

    readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This attempts to implement [grid lstm](http://arxiv.org/abs/1507.01526).
 
 Uploading to discuss and find help.
+
+Currently implements only the 1-D grid LSTM.
--- main.lua
+++ main.lua
@@ -10,10 +10,18 @@ cmd:text('Options')
 cmd:option('-size', 200,'rnn size')
 cmd:option('-k', 15, 'size of input bit vector')
 cmd:option('-n', 5, 'number of layers')
-cmd:option('-mb', 64, 'minibatch size')
-cmd:option('-iters', 100, 'number of iterations')
+cmd:option('-mb', 128, 'minibatch size')
+cmd:option('-iters', 100000, 'number of iterations')
+cmd:option('-gpu', -1, 'gpuid to use, -1 for cpu')
+cmd:option('-seed', 1, 'gpu seed')
 opt = cmd:parse(arg)
 
+if opt.gpu > 0 then
+    cunn = require 'cunn'
+    cutorch = require 'cutorch'
+    cutorch.setDevice(opt.gpu)
+    cutorch.manualSeed(opt.seed)
+end
 local x = nn.Identity()()
 
 local h0 = nn.Linear(opt.k, opt.size)(x)
@@ -27,21 +35,20 @@ local y = nn.Sigmoid()(nn.CAddTable()({
 }))
 
 local model = nn.gModule({x}, {y})
-local criterion = nn.BCECriterion()
+local criterion = nn.BCECriterion() 
 
 w, dw = model:getParameters()
 w:uniform(-0.08, 0.08)
-optim_state = {learningRate=1.}
 feval = function(w_new)
     dw:zero()
     local xx = torch.round(torch.rand(opt.mb, opt.k))
-    local yy = torch.sum(xx, 2):apply(function(i) return math.fmod(i, 2)end)
+    local yy = torch.sum(xx, 2):apply(function(i) return i % 2 end)
     local loss = criterion:forward(model:forward(xx), yy)
     model:backward(xx, criterion:backward(model.output, yy))
     return loss, dw
 end
 
 for i=1,opt.iters do
-    _, fs = optim.sgd(feval,w, optim_state)
-    print (torch.exp(fs[1]))
+    _, fs = optim.adam(feval,w)
+    if i % 2 == 0 then print (torch.exp(fs[1])) end
 end 
