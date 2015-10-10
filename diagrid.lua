9b42d7976018024cb46929906a61cfe06c0cb7a8
Nikita Gupta <gnikita@google.com>
2015-10-09 18:13:40 -0700

    add new diagrid

--- diagrid.lua
+++ diagrid.lua
@@ -3,7 +3,7 @@ local diagrid = {}
 function diagrid.diagrid(size, n, prev_h, prev_m)
     local h = prev_h
     local m = prev_m
-    for i=1,n do
+    for i=3,n do
         h, m = lstm.lstm(size, h, m)
     end
     return h, m
